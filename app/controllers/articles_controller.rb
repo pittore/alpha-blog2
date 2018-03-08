d9569890cfc3158d8d2c698c8b9576d090cffb36
Jerry Ross <pittore44@yahoo.com>
2018-03-08 18:05:05 +0000

    added new gem and nav

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,10 @@
 class ArticlesController < ApplicationController
+    
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
   
     def index
         @articles = Article.all
     end
-  
-  
-  
   
     def new
         @article = Article.new
@@ -30,8 +29,15 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was succesfully deleted"
+        #redirect_to article_path
+    end
+    
     def update
-         @article = Article.find(params[:id])
+         
         if @article.update(article_params)
             flash[:notice] = "Article was succesfully created"
             redirect_to article_path(@article)
@@ -42,8 +48,12 @@ class ArticlesController < ApplicationController
     
     
     private
+    
+       def set_article
+         @article = Article.find(params[:id])
+       end
+       
        def article_params
             params.require(:article).permit(:title, :description) 
        end
-   
-end
+    end
